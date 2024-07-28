cc3e5b7f4d775f7a598d4c48b68a048098f98670
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-07-28 16:03:45 -0400

    Create Program.cs
    committing to main

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            // Person 1 
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate:");
+            double hourlyRate1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hourly worked per week:");
+            double hoursPerWeek1 = Convert.ToDouble(Console.ReadLine());
+
+            // Person 2
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate:");
+            double hourlyRate2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hourly worked per week:");
+            double hoursPerWeek2 = Convert.ToDouble(Console.ReadLine());
+
+            //Calculating annual salaries
+            double annualSalary1 = hourlyRate1 * hoursPerWeek1 * 52;
+            double annualSalary2 = hourlyRate2 * hoursPerWeek2 * 52;
+
+            //Comparing Salaries
+            bool doesPerson1MakeMore = annualSalary1 > annualSalary2;
+            Console.WriteLine($"Does Person 1 make more money than Person2?{doesPerson1MakeMore}");
+            Console.ReadLine();
+        } 
+    }
+}
