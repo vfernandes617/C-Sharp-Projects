9d44839402878fc2ac0be545220cb7c3f6e91e15
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-07-29 19:31:54 -0400

    update

--- Program.cs
+++ Program.cs
@@ -27,10 +27,14 @@ namespace ConsoleApp1
             double annualSalary1 = hourlyRate1 * hoursPerWeek1 * 52;
             double annualSalary2 = hourlyRate2 * hoursPerWeek2 * 52;
 
+            //Annual salaries
+            Console.WriteLine("Annual Salary of Person 1:"+ annualSalary1);
+            Console.WriteLine("Annual salary of Person 2:" + annualSalary2);
+
             //Comparing Salaries
             bool doesPerson1MakeMore = annualSalary1 > annualSalary2;
             Console.WriteLine($"Does Person 1 make more money than Person2?{doesPerson1MakeMore}");
-            Console.ReadLine();
+            Console.ReadLine(doesPerson1MakeMore);
         } 
     }
 }
