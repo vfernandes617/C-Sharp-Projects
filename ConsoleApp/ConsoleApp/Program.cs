40542eac8690536d1f9b59f5e123a20f5a9abfa1
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-10 17:53:44 -0400

    Console app assignment

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //One-dimensional array of strings
+            string[] Fruits = new string[] { "Bannana", "strawberries", "cherries", "pear", "apple", "melon" };
+
+            Console.WriteLine("Enter a fruit name:");
+            string userInput = Console.ReadLine();
+
+            // Loop through the array and append user input
+            for (int i = 0; i < Fruits.Length; i++)
+            {
+                Fruits[i] += " " + userInput;
+            }
+
+            Console.WriteLine("\nYour smoothy mix is");
+            foreach (string text in Fruits)
+            {
+                Console.WriteLine(text);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
