14cbaab39bb51ede73e3bc5d27a96dc39a56e2ad
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-20 22:00:36 -0400

    for assignment method

---
+++ METHOD SUBMISSION/METHOD SUBMISSION/MathOps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_SUBMISSION
+{
+    internal class MathOps
+    {
+        //created class with a method that takes two integers as parameters. numX and numY
+        public int test(int numX, int numY = 20)
+        { 
+            return numX + numY; 
+        }
+    }
+}
---
+++ METHOD SUBMISSION/METHOD SUBMISSION/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_SUBMISSION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class 'MathOps'
+            MathOps mathOps = new MathOps();
+
+            //Ask user to input two numbers one at a time
+            Console.WriteLine("Enter in a number");
+            int UserInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter in a second number if you like press enter to skip.");
+            string secondNumber = Console.ReadLine();
+
+            int result;
+
+            //calling the method in the class 
+            if (string.IsNullOrEmpty(secondNumber))
+            {
+                result = mathOps.test(UserInput);
+
+            }
+            else
+            {
+                int secondNumberByUser = int.Parse(secondNumber);
+                result = mathOps.test(UserInput, secondNumberByUser);
+
+            }
+            Console.WriteLine("The results is:" + result);
+            Console.ReadLine();
+        }
+    }
+}
