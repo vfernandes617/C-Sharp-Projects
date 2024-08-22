6c3ed92f23354972c032a973a0eb5cb62772fbc7
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-21 21:49:01 -0400

    Method assignment

---
+++ METHOD CLASS/METHOD CLASS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Voidmethod voidmethod = new Voidmethod();
+
+            //Calling method in class passing two numbers
+
+            Console.WriteLine("Enter in a number");
+
+            Voidmethod.Display(3, 10);
+
+            //Calling Method
+            Console.WriteLine("\nCalling the method:");
+                Voidmethod.Display(num1:20, num2:20);
+        }
+ 
+    }
+    
+}
---
+++ METHOD CLASS/METHOD CLASS/Voidmethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS
+{
+    internal class Voidmethod
+    {
+        public static void Display (int num1, int num2)
+        {
+            int result = num1 * num2;
+
+            Console.WriteLine("Here is the results" + result);
+        }
+
+        
+    }
+}
