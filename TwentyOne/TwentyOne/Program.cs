10831a266c6ff8a3830d794c122b580cc903b15f
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-22 22:11:24 -0400

    updaTE

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -13,50 +13,30 @@ namespace TwentyOne
         {
       
            Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 3);
+            
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled {0}", timesShuffled);
             Console.ReadLine();
 
             Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck,out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> TempList = new List<Card>();
-                Random random = new Random();
 
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    TempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
 
-                }
-                deck.Cards = TempList;
-
-            }
-            return deck;
-
-        }
+    }
             
            
-        }
+}
 
        
 
 
-    }
+    
 
  
