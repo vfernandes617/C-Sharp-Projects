0c4a5c90846c5695aa1ab1af9aa18757e74e45cc
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-23 12:38:59 -0400

    Assignment

---
+++ METHODS AND OBJECTS SUBMISSION ASSIGNMENT/METHODS AND OBJECTS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHODS_AND_OBJECTS_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            person.SayName();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,20 +11,25 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+           TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Valter", "Joe", "Sally" };
+            game.ListPlayers();
+            game.Play();
+            Console.ReadLine();
       
-           Deck deck = new Deck();
+           //Deck deck = new Deck();
             
-            deck.Shuffle(3);
+           // deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+           // foreach (Card card in deck.Cards)
+           // {
+           //     Console.WriteLine(card.Face + " of " + card.Suit);
+           // }
+           // Console.WriteLine(deck.Cards.Count);
+           // Console.ReadLine();
 
-            Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
-            Console.ReadLine();
+           // Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+           // Console.ReadLine();
         }
 
 
