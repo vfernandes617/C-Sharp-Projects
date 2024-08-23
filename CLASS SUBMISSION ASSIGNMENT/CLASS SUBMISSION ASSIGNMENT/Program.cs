453be8826eeb11904f752607e8bda2707cec1655
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-22 22:19:06 -0400

    updayte

---
+++ CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidINT voidINT = new VoidINT();
+
+            Console.WriteLine("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+
+          
+            voidINT.WrongAnswer(num);
+
+            // Call the method with an output parameter
+            int result;
+            voidINT.WrongAnswer(num, out result);
+            Console.WriteLine("The result is: " + result);
+
+            // Call the overloaded method
+            voidINT.DivideByTwoAgain(5.5);
+        }
+    }
+}
---
+++ CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/VoidINT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT
+{
+    internal class VoidINT
+    {
+        // Method without output parameter
+        public void WrongAnswer(int number)
+        {
+            Console.WriteLine("Processing number without output parameter: " + number);
+        }
+
+        // Method with output parameter
+        public void WrongAnswer(int number, out int result)
+        {
+            result = number * 2; // Example logic for the method
+        }
+
+        // Overloaded method that accepts a double
+        public void DivideByTwoAgain(double number)
+        {
+            Console.WriteLine("Divide by two: " + (number / 2));
+        }
+    }
+}
