7e793f45fd712f0d89ad1bb1f23116e3bea54a6a
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-27 23:03:55 -0400

    PARAMETERS SUBMISSION ASSIGNMENT

---
+++ PARAMETERS SUBMISSION ASSIGNMENT/PARAMETERS SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARAMETERS_SUBMISSION_ASSIGNMENT
+{//class with gerneric parameter type
+    public class Employee<T>
+    {
+        //adding a property to the employee type called things and having it data type be a generic list
+        public List<T> Things { get; set; } = new List<T>();
+    }
+}
---
+++ PARAMETERS SUBMISSION ASSIGNMENT/PARAMETERS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARAMETERS_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the employee object with string as its generic parameter 
+            Employee<string> employee = new Employee<string>();
+            employee.Things.Add("Books");
+            employee.Things.Add("Pens");
+
+            //Instantiates Employee as an int type
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.Things.Add(10);
+            intEmployee.Things.Add(20);
+            intEmployee.Things.Add(30);
+
+            // Print the "Things" of both employees
+            Console.WriteLine("String Employee Things:");
+            Console.ReadLine();
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\nInt Employee Things:");
+            foreach (int thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
