576c48b611e16f68829438d3bd2a9b48274045ae
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-08-27 22:09:12 -0400

    Update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,12 +11,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Valter";
-            game += player;
-            game -= player;
 
             Deck deck = new Deck();
             deck.Shuffle(3);
